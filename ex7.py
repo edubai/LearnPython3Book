5b6b4065d4464ed038e75af3d83817052969b21f
Mohamed AlFalasi <ddxxbb@gmail.com>
2021-09-21 14:45:23 +0400

    comments modified

--- ex7.py
+++ ex7.py
@@ -16,6 +16,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-#Watch end = ' ' at the end. try removing it to see what happens
+#Watch end = ' ' at the end. try removing it to see what happenss
 print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
 print(end7 + end8 + end9 + end10 + end11 + end12)
