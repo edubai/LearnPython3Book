6578ac910d1dd951ecc10f9a4a20a33c54faa029
Mohamed AlFalasi <ddxxbb@gmail.com>
2021-09-23 19:55:06 +0400

    age.py

---
+++ age.py
@@ -0,0 +1,10 @@
+import datetime
+
+year_born = int(input("What year were you born? "))
+now = datetime.datetime.now()
+age = now.year - year_born
+
+
+
+#print(now.year, now.month, now.day, now.hour, now.minute, now.second)
+print(f"You are {age} years old.")
